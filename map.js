d7b42172a0f28a14f1eff08e0e93138993b8c676
DumuziT <71425551+DumuziT@users.noreply.github.com>
2023-06-27 15:40:21 +0000

    Actualizar map.js

--- map.js
+++ map.js
@@ -5,36 +5,50 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const UChile =
+const UChile1_cancha =
 [
- [
-  [-33.468569, -70.595009],[-33.468587, -70.594674],[-33.468743, -70.594684],[-33.468732, -70.595017]
- ],
- [
-  [-33.468757, -70.595017],[-33.46877, -70.594684],[-33.468933, -70.59469],[-33.468927, -70.595025]
- ],
- [
-  [-33.468951, -70.595025],[-33.468958, -70.59469],[-33.469132, -70.594706],[-33.469117, -70.595036]
- ],
- [
-  [-33.469152, -70.595036],[-33.469163, -70.594701],[-33.469327, -70.594712],[-33.469320, -70.595047]
- ]
+ [-33.468569, -70.595009],[-33.468587, -70.594674],[-33.468743, -70.594684],[-33.468732, -70.595017]
 ];
-const UTEM =
+const UChile2_cancha =
+[
+  [-33.468757, -70.595017],[-33.46877, -70.594684],[-33.468933, -70.59469],[-33.468927, -70.595025]
+];
+const UChile3_cancha =
+[
+  [-33.468951, -70.595025],[-33.468958, -70.59469],[-33.469132, -70.594706],[-33.469117, -70.595036]
+];
+const UChile4_cancha =
+[
+  [-33.469152, -70.595036],[-33.469163, -70.594701],[-33.469327, -70.594712],[-33.469320, -70.595047]
+];
+
+const UTEM_cancha =
 [
  [-33.465853, -70.596914],[-33.465863, -70.596701],[-33.465971, -70.596710],[-33.465961, -70.596923]
 ];
-const Alerces =
+
+const Alerces_cancha =
 [
  [-33.471098, -70.593536],[-33.470704, -70.592877],[-33.47104, -70.592603],[-33.47142, -70.593274]
 ];
 
-var polygon1 = L.polygon(UChile, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-polygon1.bindPopup("Ocupado");
-var polygon2 = L.polygon(UTEM, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-polygon2.bindPopup("Ocupado");
-var polygon3 = L.polygon(Alerces, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-polygon3.bindPopup("Ocupado");
+var UChile1 = L.polygon(UChile1_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+UChile1.bindPopup("Ocupado");
+
+var UChile2 = L.polygon(UChile2_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+UChile2.bindPopup("Ocupado");
+
+var UChile3 = L.polygon(UChile3_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+UChile3.bindPopup("Ocupado");
+
+var UChile4 = L.polygon(UChile4_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+UChile4.bindPopup("Ocupado");
+
+var UTEM = L.polygon(UTEM_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+UTEM.bindPopup("Ocupado");
+
+var Alerces = L.polygon(Alerces_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
+Alerces.bindPopup("Ocupado");
 
 /*
 var popup = L.popup()
