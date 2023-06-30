8489ea70dd6c3bddfdc2e8ba72af6826df99f865
DumuziT <71425551+DumuziT@users.noreply.github.com>
2023-06-30 06:00:01 +0000

    Update map.js

--- map.js
+++ map.js
@@ -1,100 +1,60 @@
 //https://leafletjs.com/reference.html
 const map = L.map('map').setView([-33.4681, -70.5946], 17);
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const UChile1_cancha =
-[
- [-33.468569, -70.595009],[-33.468587, -70.594674],[-33.468743, -70.594684],[-33.468732, -70.595017]
-];
-const UChile2_cancha =
-[
-  [-33.468757, -70.595017],[-33.46877, -70.594684],[-33.468933, -70.59469],[-33.468927, -70.595025]
-];
-const UChile3_cancha =
-[
-  [-33.468951, -70.595025],[-33.468958, -70.59469],[-33.469132, -70.594706],[-33.469117, -70.595036]
-];
-const UChile4_cancha =
-[
-  [-33.469152, -70.595036],[-33.469163, -70.594701],[-33.469327, -70.594712],[-33.469320, -70.595047]
+const UChile1_cancha = [
+  [-33.468569, -70.595009],
+  [-33.468587, -70.594674],
+  [-33.468743, -70.594684],
+  [-33.468732, -70.595017]
 ];
 
-const UTEM_cancha =
-[
- [-33.465853, -70.596914],[-33.465863, -70.596701],[-33.465971, -70.596710],[-33.465961, -70.596923]
+const UChile2_cancha = [
+  [-33.468757, -70.595017],
+  [-33.46877, -70.594684],
+  [-33.468933, -70.59469],
+  [-33.468927, -70.595025]
 ];
 
-const Alerces_cancha =
-[
- [-33.471098, -70.593536],[-33.470704, -70.592877],[-33.47104, -70.592603],[-33.47142, -70.593274]
+const UChile3_cancha = [
+  [-33.468951, -70.595025],
+  [-33.468958, -70.59469],
+  [-33.469132, -70.594706],
+  [-33.469117, -70.595036]
 ];
 
-var UChile1 = L.polygon(UChile1_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-UChile1.bindPopup("Ocupado");
+const UChile4_cancha = [
+  [-33.469152, -70.595036],
+  [-33.469163, -70.594701],
+  [-33.469327, -70.594712],
+  [-33.469320, -70.595047]
+];
 
-var UChile2 = L.polygon(UChile2_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-UChile2.bindPopup("Ocupado");
+const UTEM_cancha = [
+  [-33.465853, -70.596914],
+  [-33.465863, -70.596701],
+  [-33.465971, -70.596710],
+  [-33.465961, -70.596923]
+];
 
-var UChile3 = L.polygon(UChile3_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-UChile3.bindPopup("Ocupado");
+const Alerces_cancha = [
+  [-33.471098, -70.593536],
+  [-33.470704, -70.592877],
+  [-33.47104, -70.592603],
+  [-33.47142, -70.593274]
+];
 
-var UChile4 = L.polygon(UChile4_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-UChile4.bindPopup("Ocupado");
-
-var UTEM = L.polygon(UTEM_cancha, {color: '#66dbff', fillColor: '#66dbff', fillOpacity: 0.5}).addTo(map);
-UTEM.bindPopup("Ocupado");
-
-color = ["#ff0000","#66dbff"];
-function colors(col=color[0]){
-    return col;
+function createPolygon(coordinates, name){
+  const polygon = L.polygon(coordinates, {color: 'cyan'}).addTo(map);
+  polygon.bindPopup(`${name}`);
 }
 
-//var Alerces = L.polygon(Alerces_cancha, {color: colors(), fillColor: colors(), fillOpacity: 0.5});
-
-var xxx = 0;
-
-var Alerces;
-var cancha = [Alerces];
-var canchaa = [Alerces_cancha];
-
-function canchas(cancha,canchaa){
-    cancha = L.polygon(canchaa, {color: colors(), fillColor: colors(), fillOpacity: 0.5});
-    cancha.addTo(map);
-    console.log("bien");
-}
-
-function allCanchas(){
-    if(xxx==0){
-        xxx++;
-console.log(xxx);
-        for(var i=0;i<cancha.length;i++){
-            canchas(cancha[i], canchaa[i]);
-        }
-    setTimeout(() => {allCanchas();}, 5000);
-    }else{
-console.log(xxx);
-        cancha.remove();
-        for(var i=0;i<cancha.length;i++){
-            canchas(cancha[i], canchaa[i]);
-        }
-    setTimeout(() => {allCanchas();}, 5000);
-    }
-}
-allCanchas();
-
-
-/*
-var popup = L.popup()
-	.setLatLng()
-
-function onMapClick(e){
-	popup
-		.setLatLng(e.latlng)
-		.setContent(`${e.latlng.toString()}`)
-		.openOn(map);
-}
-map.on('click', onMapClick);
-*/
+createPolygon(UChile1_cancha, 'UChile1');
+createPolygon(UChile2_cancha, 'UChile2');
+createPolygon(UChile3_cancha, 'UChile3');
+createPolygon(UChile4_cancha, 'UChile4');
+createPolygon(UTEM_cancha, 'UTEM');
+createPolygon(Alerces_cancha, 'Alerces');
