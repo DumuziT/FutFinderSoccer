65a3779f3741aaa0886725023b817f22584b6a33
DumuziT <71425551+DumuziT@users.noreply.github.com>
2023-07-08 06:53:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-sudo apt update
 sudo apt-get update
+sudo apt-get install python3 -y
 sudo apt-get install openssl -y
-sudo apt update
-sudo apt-get update
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -sha256 -key key.pem -out certificate.pem -days 365
-
