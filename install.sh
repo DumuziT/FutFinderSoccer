15ae673758a19a1da342629db80276ca944d1ca0
DumuziT <71425551+DumuziT@users.noreply.github.com>
2023-07-08 07:09:18 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install python3 -y
-sudo apt-get install openssl -y
-openssl genrsa -out key.pem 2048
-openssl req -new -x509 -sha256 -key key.pem -out certificate.pem -days 365
